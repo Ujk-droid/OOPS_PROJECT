3b4cd017c188c1b67053d34f226a092fbf1a533b
ujk-droid <03312436713aa@gmail.com>
2024-07-09 05:56:34 -0700

    first commit

---
+++ index.ts
@@ -0,0 +1,59 @@
+import inquirer from "inquirer";
+
+class Student {
+    name: string;
+    constructor(n: string) {
+        this.name = n;
+    }
+}
+
+class Person {
+    students: Student[] = [];
+    addStudent(obj: Student) {
+        this.students.push(obj);
+    }
+}
+
+const persons = new Person();
+
+const programStart = async (persons: Person) => {
+    console.log("Welcome");
+    const ans = await inquirer.prompt([
+        {
+            name: "select",
+            type: "list",
+            message: "Whom would you like to interact with?",
+            choices: ["staff", "student", "exit"]
+        }
+    ]);
+
+    if (ans.select === "staff") {
+        console.log("You approach the staff room. Please feel free to ask any questions.");
+    } else if (ans.select === "student") {
+        const ansStudent = await inquirer.prompt([
+            {
+                name: "student",
+                type: "input",
+                message: "Enter the student's name you wish to engage with."
+            }
+        ]);
+
+        let student = persons.students.find(val => val.name === ansStudent.student);
+        
+        if (!student) {
+            student = new Student(ansStudent.student);
+            persons.addStudent(student);
+            console.log(`Hello, I am ${student.name}. Nice to meet you!`);
+            console.log("New student list:");
+            console.log(persons.students);
+        } else {
+            console.log(`Hello, I am ${student.name}. Nice to see you again!`);
+            console.log("Existing student list:");
+            console.log(persons.students);
+        }
+    } else if (ans.select === "exit") {
+        console.log("Exiting the program...");
+    }
+};
+
+programStart(persons);
